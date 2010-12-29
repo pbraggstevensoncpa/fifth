bc559b824e255d0df35362c93500a258bb90bb69
pbraggstevensoncpa <p.bragg.stevenson.cpa@gmail.com>
2010-12-29 11:25:03 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fifth::Application.config.secret_token = '47fca9c9bdc0416bfe546884b97cbbf3771328983b97f58e6739aa860db9ea97467b5294465667e28368cfa03785363afc1f6e3c553c94d94db5e2bead6a93f2'
